3250996bf4ef74510fdd91a968f4f227784086f3
Kalp Gandhi <kalpgandhi022@gmail.com>
2024-05-27 22:20:57 +0530

    initial commit

---
+++ src/main/java/com/example/storefinder_application/StorefinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.storefinder_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorefinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storefinder_application/StorefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storefinder_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
